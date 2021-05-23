f0ab45e69042189bb56098f4a0e245a54c997375
piyush8000 <84653188+piyush8000@users.noreply.github.com>
2021-05-23 17:10:13 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇ ꜱᴏɴɢ ʙʏ @P_4_PEEYUSH...")
+    m.edit("ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇ ꜱᴏɴɢ ʙʏ @piyush_raj007...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ @sharkmusicbot**'
+        rep = '**🎵 ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ @GodzillaMusic_Bot**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
